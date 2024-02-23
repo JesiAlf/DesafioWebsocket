b7ec75450a502414615e7588f66864faf6f045a7
Jesica Alfonso <jesica.l.alfonso@gmail.com>
2024-02-22 21:29:34 -0300

    DesafioWebsocket

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import { Server } from "socket.io";
 import handlebars from "express-handlebars";
 import viewsRouter from "./routes/viewsRouter.js";
-import path from "path";
+import realTimeProducts from "./routes/realTimeProductsRouter.js";
+//import path from "path";
 import express from "express";
 import { ProductManager } from "./manager/productsManager.js";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import realTimeProducts from "./routes/realTimeProductsRouter.js";
 //import cartsRouter from"./routes/carts.router.js";
 //import productRouter from"./routes/productRouter.js";
 
@@ -24,34 +24,33 @@ const socketServer = new Server(httpServer);
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(__dirname + "/src/public"));
 
 //Routes
 //app.use('/api/products/', productRouter)
 //app.use('/api/carts/', cartsRouter)
 
-app.use("/api",viewsRouter);
-app.use("/realTimeProducts",realTimeProducts)
+app.use("/", viewsRouter);
+app.use("/realTimeProducts", realTimeProducts);
 //estructura codigo handlebans_
 
 app.engine("handlebars", handlebars.engine());
 //tenemos que setear, debemos decir q nuestro views engines y la extencion de los archivo estan en handlebars._
 
-app.set("view engine","handlebars");
+app.set("view engine", "handlebars");
 //luego decirles donde estan esos archivos_
 
-app.set("views", path.join(__dirname + " /views"));
+app.set("views", __dirname +"/views");
 
 //conexiion con socket.io_
- socketServer.on("connection", (socket) => {
+socketServer.on("connection", (socket) => {
   console.log("Nueva conexion");
   try {
     const products = p.getProducts();
     socketServer.emit("products", products);
-} catch (error) {
-    socketServer.emit('response', { status: 'error', message: error.message });
-}
-
+  } catch (error) {
+    socketServer.emit("response", { status: "error", message: error.message });
+  }
 
   socket.on("newProduct", async (newProduct) => {
     try {
@@ -67,12 +66,16 @@ app.set("views", path.join(__dirname + " /views"));
         Product
       );
       const pushId = pushNewProduct.id;
-      socketServer.emit("products", Product)
-      socketServer.emit("responde", { status: "success",
+      socketServer.emit("products", Product);
+      socketServer.emit("responde", {
+        status: "success",
         message: `product ${pushId}successfully added`,
       });
     } catch (error) {
-      socketServer.emit("responde", { status: "error", message: error.message });
+      socketServer.emit("responde", {
+        status: "error",
+        message: error.message,
+      });
     }
   });
 
@@ -87,7 +90,10 @@ app.set("views", path.join(__dirname + " /views"));
         message: "Product delete successfully",
       });
     } catch (error) {
-      socketServer.emit("responde", { status: "error", message: error.message });
+      socketServer.emit("responde", {
+        status: "error",
+        message: error.message,
+      });
     }
   });
 });
--- src/routes/realTimeProductsRouter.js
+++ src/routes/realTimeProductsRouter.js
@@ -3,25 +3,15 @@ import { ProductManager } from "../manager/productsManager.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => { 
-  const p= new ProductManager();
-  const Product= p.getProduct();
+router.get("/", async (req, res) => { 
+  //const p= new ProductManager();
+  const Product= await ProductManager.getProduct();
   res.render("realTimeProduts",{
     Product
   });
 });
-  /*try {
-    const products = await ProductManager.getInstance().getProducts();
-    res.render("realTimeProducts", {
-      title: "real time products",
-      products: products,
-    });
-  } catch (error) {
-    console.error("Error al obtener los productos:", error);
-    res.status(500).send("Error del servidor");
-  }*/
-
-router.post("/", (req, res) => {
+  
+/*router.post("/", (req, res) => {
   const p= new ProductManager();
   const addProduct= p.addProduct();
   res.render("realTimeProduts",{
@@ -34,18 +24,7 @@ router.post("/", (req, res) => {
     res.render("realTimeProduts",{
       deleteProduct
     });
-/*router.post("/", async (req, res) => {
-  try {
-    const newProduct = req.body;
-    await ProductManager.getInstance().addProduct(newProduct);
-    const updateProduct = await ProductManager.getInstance().getProducts();
-    io.emit("newProduct", updateProduct);
-    res
-      .status(200)
-      .json({ status: "success", message: "Producto agregado exitosamente" });
-  } catch (error) {
-    res.status(500).json({ status: "error", message: error.message });
-  }*/
-});
+
+});*/
 
 export default router;
